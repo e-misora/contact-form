01252db6771ee3863b1a50d9544841f593cf9789
misora <meikongjiangteng803@gmail.com>
2025-11-22 09:46:36 +0900

    教材終わり

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
+Route::get('/',[ContactController::class,'index']);
+Route::get('/contacts/confirm',[ContactController::class,'confirm']);
+Route::post('/contacts/confirm',[ContactController::class,'confirm']);
+Route::get('/contacts',[ContactController::class,'store']);
+Route::post('/contacts',[ContactController::class,'store']);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +19,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
